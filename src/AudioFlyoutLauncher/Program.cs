05c18b67b087fd72720636b4f762a325db12ea2f
ADeltaX <info@adeltax.com>
2019-08-01 16:05:33 +0200

    Admin not required, Launcher for ShellExecute launch (for service/task launch)

---
+++ src/AudioFlyoutLauncher/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioFlyoutLauncher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var file = AppDomain.CurrentDomain.BaseDirectory + "AudioFlyoutUA.exe";
+                ProcessStartInfo psi = new ProcessStartInfo();
+                psi.UseShellExecute = true;
+                psi.FileName = file;
+                Process.Start(psi);
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
